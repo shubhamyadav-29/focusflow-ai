180cc80f1984bf0d12a75beecf127a38680ef4ad
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-03-17 16:35:12 +0530

    Day-3: Notes form with useState and list rendering

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,24 @@
 import { NavLink } from "react-router-dom";
- 
-export default function Sidebar(){
-    return (
-        <div style={{width:"200px",background:"#111",color:"#fff",height:"100vh"
-        }}><h2>Focus Flow</h2>
 
-        <nav>
-            <NavLink to="/">Dashboard</NavLink><br/>
-            <NavLink to="/notes">Notes</NavLink><br/>
-            <NavLink to="/tasks">Tasks</NavLink>
-        </nav>
-        </div>
-    )
+export default function Sidebar() {
+  return (
+    <div
+      style={{
+        width: "200px",
+        background: "#111",
+        color: "#fff",
+        height: "100vh",
+      }}
+    >
+      <h2>Focus Flow</h2>
+
+      <nav>
+        <NavLink to="/">Dashboard</NavLink>
+        <br />
+        <NavLink to="/notes">Notes</NavLink>
+        <br />
+        <NavLink to="/tasks">Tasks</NavLink>
+      </nav>
+    </div>
+  );
 }
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,3 +1,26 @@
+import  { useState } from 'react'
+
 export default function Notes() {
-    return <h1>Notes Page</h1>
+    const [note,setNote]=useState("")
+    const [notesList,setNoteList]=useState([])
+
+    const handleAddNote = () =>{
+        setNoteList([...notesList,note])
+        setNote("")
+    }
+    return (<div>
+       <h1>Notes</h1>
+       <input type="text" value={note} onChange={(e) => setNote(e.target.value)}
+       placeholder="Write note.." />
+
+       <button onClick={handleAddNote}>Add</button>
+
+       <ul>
+        {notesList.map((n,index) => (
+            <li key={index}>{n}</li>
+        ))}
+       </ul>
+
+    </div>
+    )
 }
