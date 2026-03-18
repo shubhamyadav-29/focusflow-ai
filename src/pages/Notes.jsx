585a370d28540ff7cfc14a2dc59fc695c7af2271
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-03-18 15:43:09 +0530

    Day-4: useEffect localStorage and delete feature

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,12 +1,31 @@
-import  { useState } from 'react'
+import  { useEffect, useState } from 'react'
 
 export default function Notes() {
     const [note,setNote]=useState("")
     const [notesList,setNoteList]=useState([])
 
+
+    useEffect(()=>{
+        const savedNotes = JSON.parse(localStorage.getItem("notes"))
+        
+        if(savedNotes){
+            setNoteList(savedNotes)
+        }
+      
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("notes",JSON.stringify(notesList))
+    },[notesList])
+
     const handleAddNote = () =>{
+        if(note.trim()==="")return
         setNoteList([...notesList,note])
         setNote("")
+    }
+    const handleDeleteNote = (index) =>{
+        const updated = notesList.filter((_, i) => i !== index)
+        setNoteList(updated)
     }
     return (<div>
        <h1>Notes</h1>
@@ -17,7 +36,9 @@ export default function Notes() {
 
        <ul>
         {notesList.map((n,index) => (
-            <li key={index}>{n}</li>
+            <li key={index}>{n}
+            <button onClick={() => handleDeleteNote(index)}>Delete</button>
+            </li>
         ))}
        </ul>
 
