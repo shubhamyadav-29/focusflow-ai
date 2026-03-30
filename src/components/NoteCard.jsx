4b57f5fe0c89280196bad00df296217f4df027ce
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-03-30 19:21:23 +0530

    Day-16: AI note summarizer feature added

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,6 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
+import { summarizeNoteAPI } from "../services/aiService"
 
 function NoteCard({ text, onDelete }) {
+
+  const [summary, setSummary] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSummarize = async () => {
+    setLoading(true)
+    const res = await summarizeNoteAPI(text)
+    setSummary(res.data)
+    setLoading(false)
+  }
 
   return (
     <div style={{
@@ -12,6 +23,12 @@ function NoteCard({ text, onDelete }) {
       <p>{text}</p>
 
       <button onClick={onDelete}>Delete</button>
+
+      <button onClick={handleSummarize}>
+        {loading ? "Summarizing..." : "Summarize"}
+      </button>
+
+      {summary && <p><strong>Summary:</strong> {summary}</p>}
     </div>
   )
 }
---
+++ src/services/aiService.js
@@ -0,0 +1,11 @@
+export const summarizeNoteAPI = (note) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const summary = note.length > 20 
+        ? note.slice(0, 20) + "..."
+        : note
+
+      resolve({ data: summary })
+    }, 1000)
+  })
+}
