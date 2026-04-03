22c12363412378a32d38d40418c1a5fd912c7813
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-04-03 20:04:34 +0530

    Making some changes

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -1,11 +1,11 @@
-export const summarizeNoteAPI = (note) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const summary = note.length > 20 
-        ? note.slice(0, 20) + "..."
-        : note
+// export const summarizeNoteAPI = (note) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const summary = note.length > 20 
+//         ? note.slice(0, 20) + "..."
+//         : note
 
-      resolve({ data: summary })
-    }, 1000)
-  })
-}
+//       resolve({ data: summary })
+//     }, 1000)
+//   })
+// }
